34a89814a847443bee11ebcb9569af28e1551505
Ana Leone <ana.leone.rodrigues@escola.pr.gov.br>
2026-07-08 20:18:55 -0300

    script.js

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "De carro.",
-                afirmacao: "chega rápido e faz um trajeto pequeno."
+                afirmacao: "Chego rápido e faço um trajeto pequeno."
             },
             {
-                texto: "de ônibu.s",
-                afirmacao: "chega mais tarde e faz um trajeto mais longo."
+                texto: "de ônibus.",
+                afirmacao: "Chego mais tarde e faço um trajeto mais longo."
             }
         ]
     },
@@ -23,50 +23,50 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Vou para célula.",
-                afirmacao: "Minha mãe me leva, faço a comunhão, como e volto embora."
+                afirmacao: "Minha mãe me leva, faço a comunhão, me alimento e volto embora."
             },
             {
                 texto: "Vou para Chácara dos meus avós.",
-                afirmacao: "faço um trajeto de uma hora com meus pais e aproveito o resto do dia tranquilamente com meus familiares."
+                afirmacao: "Faço um trajeto de uma hora com meus pais e aproveito o resto do dia tranquilamente com meus familiares."
             }
         ]
     },
     {
-        enunciado: "O que ele fez ao chegar em destino?",
+        enunciado: "Como você estuda para uma prova",
         alternativas: [
             {
-                texto: "procurou por alguém",
-                afirmacao: "olhando em todas as direções, procurando encontrar alguém."
+                texto: "Estudo no dia anterior só.",
+                afirmacao: "Fico com preguiça e acabo deixando pra estudar em cima da hora."
             },
             {
-                texto: "Refletiu sozinho",
-                afirmacao: "observando sua volta, viu que não precisava de mais nada."
+                texto: "Estudo antecipadamente.",
+                afirmacao: "Estudo um pouco cada dia e consigo ir mais tranquila para prova."
             }
         ]
     },
     {
-        enunciado: "O que você faz no seu tempo livre?",
+        enunciado: "O que vc faz no seu tempo livre?",
         alternativas: [
             {
                 texto: "Assisto Tik Tok.",
                 afirmacao: "Perco meu tempo procastinando no Tik Tok."
             },
             {
-                texto: "Escuto podcast",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "Escuto podcast.",
+                afirmacao:"Aproveito meu tempo aprendendo coisas novas e evoluindo."
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "O que você faz quando tem algum problema?",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Eu desisto logo.",
+                afirmacao: "Perco oportunidades por não tentar resolver."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Procuro uma solução.",
+                afirmacao: "Aprendo com os desafios e ganho mais confiança. "
             }
         ]
     },
